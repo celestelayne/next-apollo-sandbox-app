267211398b52e5d076b028f2b205229a30d46d2c
Celeste Layne <layne.celeste@gmail.com>
2021-08-03 23:16:22 -0400

    set up apollo client

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
+import { gql } from '@apollo/client';
+import client from '../apollo-client';
+
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
-const Home = () => {
+const Home = ({ countries }) => {
+  console.log(countries)
   return (
     <Layout>
-      <h2>First Post</h2>
+      <h2>List of Countries</h2>
+      <div>
+        {countries.map(country => (
+          <p>{country.name} &mdash; {country.emoji}</p>
+        ))}
+      </div>
       <p>
         <Link href="/">
           Back to home
@@ -12,6 +21,25 @@ const Home = () => {
       </p>
     </Layout>
   )
+}
+
+export async function getStaticProps(){
+  const { data } = await client.query({
+    query: gql`
+      query Countries {
+        countries {
+          code
+          name
+          emoji
+        }
+      }
+    `
+  });
+  return {
+    props: {
+      countries: data.countries.slice(0, 4)
+    }
+  }
 }
 
 export default Home;
